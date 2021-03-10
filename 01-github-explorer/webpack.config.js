9e67dfca9a92b525934f02d3e6b0b45e2dc3f593
Carlos Oliveira <chopcsom@gmail.com>
2021-03-10 20:12:32 -0300

    config: Webpack

---
+++ 01-github-explorer/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.jsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/, // arquivos terminados em .jsx
+        exclude: /node_modules/, // exclui esse diretorio
+        use: "babel-loader", // integração babel + webpack
+      },
+    ],
+  },
+};
