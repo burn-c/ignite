68fc97214ff887ee55fec8000532f9188d731c7e
Carlos Oliveira <chopcsom@gmail.com>
2021-03-10 22:23:15 -0300

    feat: component state

--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,5 +1,12 @@
+import { Counter } from "./components/Counter";
 import { RepositoryList } from "./components/RepositoryList";
 
 export function App() {
-  return <RepositoryList/>
+  
+  return (
+    <>
+      <RepositoryList/>
+      <Counter />
+    </>
+  )
 }
